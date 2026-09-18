c03993c25514ce51f964acd63a33ca58e261e94d
mjf326 <157730426+mjf326@users.noreply.github.com>
2026-09-18 15:00:35 -0400

    Add files via upload

---
+++ FA_Ripple_4.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/11/2026 09:50:25 AM
+// Design Name: 
+// Module Name: FA_Ripple_4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FA_Ripple_4(input [3:0] A, input [3:0] B, input CI, output [3:0] SUM, output CO);
+
+wire c1, c2, c3; 
+
+FA_1 uut0 (.A(A[0]), .B(B[0]), .CI(CI), .SUM(SUM[0]), .CO(c1));
+FA_1 uut1 (.A(A[1]), .B(B[1]), .CI(c1), .SUM(SUM[1]), .CO(c2));
+FA_1 uut2 (.A(A[2]), .B(B[2]), .CI(c2), .SUM(SUM[2]), .CO(c3));
+FA_1 uut3 (.A(A[3]), .B(B[3]), .CI(c3), .SUM(SUM[3]), .CO(CO));
+
+
+endmodule
